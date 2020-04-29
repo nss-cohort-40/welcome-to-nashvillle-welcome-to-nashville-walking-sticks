d198fa36543278e8b838f9fbf3d9b3878ac5b2ee
zanebliss <zanebliss@icloud.com>
2020-04-29 14:48:33 -0500

    added API key variable and security

--- src/scripts/concerts-getter.js
+++ src/scripts/concerts-getter.js
@@ -1,6 +1,6 @@
 let concertsAPI =  {
         concertsGetter: function() {
-        return fetch("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=nashville&apikey=gnAHq7Kly2A1nAKhgONKMj3nC5XMGEfw")
+        return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=nashville&apikey=${concertsAPIKey}`)
         .then(events => events.json())
         }
 }
