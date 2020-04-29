6874615b0bbdefddf5c17d972f7b3d222eaa2773
zanebliss <zanebliss@icloud.com>
2020-04-29 13:51:03 -0500

    cleaning

--- src/scripts/concerts-getter.js
+++
@@ -1,6 +0,0 @@
-let concertsAPI =  {
-        concertsGetter: function() {
-        return fetch("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=nashville&apikey=gnAHq7Kly2A1nAKhgONKMj3nC5XMGEfw")
-        .then(events => events.json())
-        }
-}
--- src/scripts/itinerary.js
+++
@@ -1,7 +0,0 @@
-searchResults = document.getElementById("itinerary-display")
-rootElement.addEventListener("click", function (event) {
-    let targetElement = event.target
-    console.log(targetElement.innerHTML.split(" "[0]));
-    // element = makeInineraryComponent()
-    // document.querySelector(".itinerary-display")
-  });
