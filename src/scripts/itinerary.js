94d879ec189a6c4f4bba1b9b308f73691ba9583a
iamkaleb <kalebdmoran@gmail.com>
2020-04-30 15:12:31 -0500

    MVP of itinerary JSON

--- src/scripts/itinerary.js
+++ src/scripts/itinerary.js
@@ -1,3 +1,10 @@
+const itinObj = {
+  park: "",
+  art: "",
+  concert: "",
+  restaurant: ""
+}
+
 rootElement.addEventListener("click", function (event) {
   itineraryBox = document.querySelector(".itinerary-display");
   if (event.target.id.split("-")[0] == "save") {
@@ -11,26 +18,31 @@ rootElement.addEventListener("click", function (event) {
     if (document.getElementById("results").innerHTML == "Art Results") {
       if (itineraryBox.querySelector(".art-results").innerHTML == "") {
         document.querySelector(".art-results").innerHTML += makeItineraryComponent(artName, "art");
+        itinObj.art = artName;
       } else if (itineraryBox.querySelector("#art-result") != null) {
         resetResults("art");
         document.querySelector("#art-result").innerHTML = makeItineraryComponent(artName, "art");
+        itinObj.art = artName;
       }
     } else if (document.getElementById("results").innerHTML == "Concert Results") {
       if (itineraryBox.querySelector(".concert-results").innerHTML == "") {
         document.querySelector(".concert-results").innerHTML += makeItineraryComponent(concertName, "concert");
+        itinObj.concert = concertName;
       } else if (itineraryBox.querySelector("#concert-result") != null) {
         resetResults("concert");
         document.querySelector("#concert-result").innerHTML = makeItineraryComponent(concertName, "concert");
+        itinObj.concert = concertName;
     }
     } else if (document.getElementById("results").innerHTML == "Parks Results") {
       if (itineraryBox.querySelector(".parks-results").innerHTML == "") {
         document.querySelector(".parks-results").innerHTML += makeItineraryComponent(parkName, "parks");
+        itinObj.park = parkName;
       } else if (itineraryBox.querySelector("#parks-result") != null) {
         resetResults("parks");
         document.querySelector("#parks-result").innerHTML = makeItineraryComponent(parkName, "parks");
+        itinObj.park = parkName;
     }
     }
-
   }
 });
 
@@ -43,3 +55,15 @@ const getResult = (number) => {
   splitSentence = content.split("<button");
   return name = splitSentence[0].substr(3);
 };
+
+document.getElementById("save-itin").addEventListener("click", function () {
+  fetch("http://localhost:8088/itinerary", {
+    method: "POST",
+    body: JSON.stringify(itinObj),
+    headers: {
+      "Content-Type": "application/json"
+    },
+  })
+    .then(res => res.json())
+    // .then(res => console.log(res))
+})
